8f3cc78682aa90379df76d4f6cf6d5ef6497b682
Seattle Academy <seattleacademy@users.noreply.github.com>
2019-05-13 12:27:31 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+var WebSocketServer = require('ws').Server;
+var http = require('http');
+var express = require('express');
+var app = express();
+
+port = 1206;
+var sensors = {};
+counter = 0;
+
+app.use(express.static(__dirname + '/public'));
+
+var server = http.createServer(app);
+server.listen(port);
+console.log('listening on port', port)
+
+function getData() {
+        sensors.counter = counter++
+}
+getData();
+setInterval(getData, 100); //less that 25ms is erratic
+
+app.all('/all', function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(JSON.stringify(sensors));
+});
+
+app.all('/counter', function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(sensors.counter.toString());
+});
+
+var wss = new WebSocketServer({ server: server });
+wss.on('connection', function(ws) {
+    var id = setInterval(function() {
+        ws.send(JSON.stringify(sensors), function() { /* ignore errors */ });
+    }, 500);
+    console.log('connection to client');
+    ws.on('close', function() {
+        console.log('closing client');
+        clearInterval(id);
+    });
+});
